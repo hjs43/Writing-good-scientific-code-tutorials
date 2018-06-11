9bfd1c0dca4d58d8ea604ac52cf02844c04c4f98
Heidi <Heidi.somsel@phys.uni-goettingen.de>
2018-06-11 16:44:34 +0200

    Create and commit hello world that calls monkey

--- oh_hey_world.py
+++
@@ -1,6 +0,0 @@
-
-# -*- coding:utf-8 -*-
-if __name__ == "__main__":
-    print('Hello World')
-    print('Starting to understand Git. This is already my second commit.')
-    print('Hello world again')
