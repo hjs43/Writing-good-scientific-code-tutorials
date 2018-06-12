fd718b45e3f74b123f2425f47ff2c12d3fd2b081
Heidi <Heidi.somsel@phys.uni-goettingen.de>
2018-06-12 11:55:09 +0200

    don't know 2 - cherry pick2

--- hello_world.py
+++ hello_world.py
@@ -25,7 +25,14 @@ def print_rabbit_range(num):
 import sys
 import monkey
 if __name__ == "__main__":
+<<<<<<< HEAD
     print('Bye bye rabbits')
     monkey.print_monkeys([0,1,2,3,4,5])
     print_palmtrees(10)
     print_rabbit_range(10)
+=======
+    print('ALLLL of the rabbits')
+    monkey.print_monkeys([0,1,2,3,4,5,7,8])
+    print_palmtrees(50)
+    print_rabbit_range(50)
+>>>>>>> 8d8489d... don't know 2
