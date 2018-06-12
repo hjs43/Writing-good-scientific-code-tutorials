6cda153a2a3ae2294cc5bff47d43fb2b9e15495b
Heidi <Heidi.somsel@phys.uni-goettingen.de>
2018-06-12 14:15:52 +0200

    Adding laras crap

---
+++ goodcodetutorial/hello_world_original.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    print ('Hello World!')
+    print ('Second commit.... again')
+    print ('Apokalypse!')
+    print ('I think I understand the basic concept.')
+    print ('Really?')
