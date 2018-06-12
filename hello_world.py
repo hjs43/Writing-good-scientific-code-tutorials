7149e3736706c396732096a0c1a9a93020e944a4
Heidi <Heidi.somsel@phys.uni-goettingen.de>
2018-06-12 11:33:33 +0200

    Bye bye rabbits

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,7 @@
 
 
+
+
 # -*- coding:utf-8 -*-
 def print_palmtrees(num):
         for i in range(num):
@@ -10,10 +12,24 @@ def print_palmtrees(num):
         sys.stdout.write('\n\n')
 
 
+def print_rabbit_range(num):
+	for i in range(num):
+		sys.stdout.write(' \\/ ')
+	sys.stdout.write('\n')
+	for i in range(num):
+		sys.stdout.write(' oo ')
+	sys.stdout.write('\n')
+	for i in range(num):
+		sys.stdout.write(' ** ')
+	sys.stdout.write('\n')
+
+
+
+
 import sys
 import monkey
 if __name__ == "__main__":
-    print('Hello World')
+    print('Bye bye rabbits')
     monkey.print_monkeys([0,1,2,3,4,5])
     print_palmtrees(10)
-
+    print_rabbit_range(10)
